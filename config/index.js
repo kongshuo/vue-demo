50c49355646f5c0b3421430096983d32e514b10b
kongshuo <yuren.ks@dtyunxi.com>
2019-01-02 11:55:30 +0800

    webpack配置

--- config/index.js
+++ config/index.js
@@ -14,8 +14,9 @@ module.exports = {
       '/api': {
         target: 'http://elm.cangdu.org', // 请求的第三方接口
         changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        logLevel: 'debug',
         pathRewrite: {
-          '^/api': '/' // 这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
+          '^/api': '' // 这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
         }
       }
     },
